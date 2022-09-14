f6c75e712e1f1bd128ae83527d0252b462a9f7ce
monneratachiame <monneratachiame@gmail.com>
2022-09-14 14:27:01 +0000

    project

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,41 @@
-import React from "react";
+import React, {useState} from "react";
+
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	//tudo que esta aqui é o que eu vou executar
+	const [list, setList] = useState([]);
+		
+	const value = (event) => {
+		//console.log(event.target.value)
+		if (event.keyCode === 13) {
+			setList([...list, event.target.value]);
+			event.target.value="";
+		};
+
+		//console.log(list)
+	}
+	
+	//criar função pra tirar o valor da lista no X - como tirar um valor do array (um index especifico)
+	//const delete = (event) => {}
+	
+
+	//tudo que jogo de volta pro usuario ver
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="list-group">
+			<h1 className="test-center">
+				Todos
+			</h1>
+			<input type="text" placeholder="Whats need to be done?" size="100" onKeyDown={value}>
+			</input>
+			<div>
+				<ul className="list-group-item disabled" aria-disabled="true">
+						{list.map((item, index) => <li key={index} className={index}>{item} <span>X</span></li>)}
+				</ul>
+			</div>
 		</div>
 	);
 };
